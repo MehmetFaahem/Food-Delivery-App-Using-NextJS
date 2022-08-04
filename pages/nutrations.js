6d7b5058f389bd8c45bb60609e7db6e91963875c
Mehmet Faahem <mehmetfaahem@gmail.com>
2022-08-04 22:51:14 +0600

    Update nutrations.js

--- pages/nutrations.js
+++ pages/nutrations.js
@@ -11,7 +11,7 @@ export default function Nutration() {
             <Container sx={{
                 p: 7,
                 mt: 2,
-                pb: 100
+                pb: 50
             }}>
                 <DenseTable />
                 <Link href='/foods'>
